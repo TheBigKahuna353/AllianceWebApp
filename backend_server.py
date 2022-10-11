c94dd9d1c714aee6696a9e737bbbeaea5bc0c7eb
Jordan <jordan20022@gmail.com>
2022-10-12 00:30:25 +1300

    scripts to host

--- backend_server.py
+++ backend_server.py
@@ -1,37 +1,5 @@
-from http.client import HTTPResponse
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
+import uvicorn
 
 
-origins = [
-    "http://localhost:8000",
-    "http://localhost:7000"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-todos = [
-    ["Client 2", "Client 5", "Client 6", "Client 1", "Client 3", "Client 4"],
-]
-
-@app.get("/api/clientList", tags=["todos"])
-async def get_todos() -> dict:
-    return { "data": todos }
-
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
